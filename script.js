897eec088542e9e32b3f84888211bed616d468c7
Guddu <pranoyghosh35@gmail.com>
2024-07-31 12:06:35 +0530

    cards

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-function toggleContent(button) {
-    var allContents = document.querySelectorAll('.card-content');
-    var content = button.nextElementSibling;
 
-    allContents.forEach(function (item) {
-        if (item !== content) {
-            item.style.display = 'none';
-        }
-    });
-
-    if (content.style.display === 'block') {
-        content.style.display = 'none';
-    } else {
-        content.style.display = 'block';
-    }
-}
 
